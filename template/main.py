d3c93f9021ca70784ae7697a31ee08e03d867f84
Katrina Hung <hong.tsui.yi@gmai.com>
2025-09-03 22:59:26 +0800

    build web template

--- template/main.py
+++ template/main.py
@@ -3,6 +3,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 # GPIO.setup(LED_PIN, GPIO.OUT)
 
+# digital
 # GPIO.output(LED_PIN, GPIO.HIGH)
+
+# analog
+# p = GPIO.PWM(pin, frq)
+# p.start(duty)
+# p.ChangeDutyCycle(duty)
+# p.stop()
 
 GPIO.cleanup()
