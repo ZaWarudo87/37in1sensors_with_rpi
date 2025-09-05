d1eaf983dade57e8045be0fd129514ff10b536e0
Katrina Hung <hong.tsui.yi@gmai.com>
2025-09-05 23:43:01 +0800

    add btn+bzr

--- Button+buzzer/main.py
+++ Button+buzzer/main.py
@@ -2,14 +2,23 @@ import RPi.GPIO as GPIO
 import time
 
 BTN_PIN = 2
+BUZZER_PIN = 3
+beep = False
+bef_btn = False
+now_btn = False
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BTN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BTN_PIN, GPIO.IN)
+GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
 try:
     while True:
-        print(GPIO.input(BTN_PIN))
-        time.sleep(1)
+        now_btn = GPIO.input(BTN_PIN)
+        if now_btn != bef_btn:
+            beep = not beep
+            GPIO.output(BUZZER_PIN, beep)
+        bef_btn = now_btn
+        time.sleep(0.1)
 except KeyboardInterrupt:
     pass
 finally:
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 37in1sensors_with_rpi
+這個神奇PCB上的`S`是signal，`-`是GND。
